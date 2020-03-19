92b172bbc34649dd98ecbc3db3ecd1edb97e4f6e
Dan Foley <dan@dan-foley.com>
2020-03-19 21:55:37 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -26,7 +26,7 @@ JOIN subgenres ON subgenres.id = series.subgenre_id"
 end
 
 def select_series_title_with_most_human_characters
-  "SELECT series.title, characters.name, characters.species, COUNT(characters.species) FROM series
+  "SELECT series.title FROM series
 JOIN characters ON series.author_id = characters.author_id
 WHERE characters.species = 'human'
 GROUP BY series.title
