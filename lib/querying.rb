d407c2b75245bacf695921921271d14f96007585
Dan Foley <dan@dan-foley.com>
2020-03-19 21:35:15 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -22,8 +22,7 @@ end
 def select_name_and_series_subgenres_of_authors
   "SELECT authors.name, subgenres.name FROM authors
 JOIN series ON authors.id = series.author_id
-JOIN subgenres
-GROUP BY authors.name"
+JOIN subgenres ON subgenres.id = series.subgenre_id"
 end
 
 def select_series_title_with_most_human_characters
